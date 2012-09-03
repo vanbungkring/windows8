ed311c5144a106858dd2e22b217c4a6a8788864b
Arie <arie@wego.com>
2012-09-03 16:24:51 +0700

    adding some function to load JSON REST

--- Wego/Wego/js/default.js
+++ Wego/Wego/js/default.js
@@ -4,36 +4,41 @@
     "use strict";
 
     var app = WinJS.Application;
-    var activation = Windows.ApplicationModel.Activation;
-    WinJS.strictProcessing();
-    function initMain(eventInfo) {
-        var msg = new Windows.UI.Popups.MessageDialog("GoodBye, see you soon!");
-        msg.showAsync().then();
+
+    function initDataSource() {
+        var url = 'http://vanbungkring.com/api/get_recent_posts/';
+        WinJS.xhr({ url: url }).then(function (data) {
+            var result = JSON.parse(data.responseText);
+            var msg = new Windows.UI.Popups.MessageDialog(result.posts[1].id);
+            msg.showAsync().then();
+
+        });
     }
 
-    app.onactivated = function (args) {
-        if (args.detail.kind === activation.ActivationKind.launch) {
-            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-                // TODO: This application has been newly launched. Initialize
+    app.onactivated = function (eventObject) {
+        if (eventObject.detail.kind === Windows.ApplicationModel.Activation.ActivationKind.launch) {
+            if (eventObject.detail.previousExecutionState !== Windows.ApplicationModel.Activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize 
                 // your application here.
+                submit.addEventListener('click', function (e) {
+                    initDataSource();
+                })
+
+                WinJS.UI.processAll();
             } else {
-                // TODO: This application has been reactivated from suspension.
+                // TODO: This application has been reactivated from suspension. 
                 // Restore application state here.
             }
-            submit.addEventListener('click', function (e) {
-                initMain();
-            })
-            args.setPromise(WinJS.UI.processAll());
         }
     };
 
-    app.oncheckpoint = function (args) {
+    app.oncheckpoint = function (eventObject) {
         // TODO: This application is about to be suspended. Save any state
-        // that needs to persist across suspensions here. You might use the
+        // that needs to persist across suspensions here. You might use the 
         // WinJS.Application.sessionState object, which is automatically
         // saved and restored across suspension. If you need to complete an
         // asynchronous operation before your application is suspended, call
-        // args.setPromise().
+        // eventObject.setPromise(). 
     };
 
     app.start();
